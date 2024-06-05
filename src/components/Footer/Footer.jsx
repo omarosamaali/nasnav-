5711a37fa60f5f4b63befc675ce5294a0322e8a1
omar <987omar123osama@gmail.com>
2024-06-05 14:44:43 +0300

    omar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,137 +1,155 @@
 import React from "react";
-import './Footer.css';
-// import footerLogo from ''
+import "./Footer.css";
+import footerLogo from "../../../public/images/Group 1431.svg";
+import facebook from "../../../public/images/Group 1432.svg";
+import linedin from "../../../public/images/Group 1435.svg";
+import insta from "../../../public/images/Group 1433.svg";
+import twiteer from "../../../public/images/Group 1434.svg";
+import nasnavfooter from "../../../public/images/Group 1439.svg";
+import Rectangle from "../../images/Rectangle 586.png";
+import visa from "../../images/Group 1437.png";
+import payment from "../../images/Group 1438.png";
 export function Footer() {
   return (
     <>
-<footer
-  class="bg-[#3C3C3B] text-white text-center text-surface/75 dark:bg-neutral-700 dark:text-white/75 lg:text-left">
-  
+      <footer className="px-20 bg-[#3C3C3B] text-white text-center text-surface/75 dark:bg-neutral-700 dark:text-white/75 lg:text-left">
+        <div className="container max-w-[1390px] mx-auto py-10 text-center md:text-left">
+          <div className="edit-grid grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-2">
+            <div className="bordernone border-r mr-[19px] border-[#5F5F5F] ">
+              <h6 className="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start">
+                <span className="me-3 [&>svg]:h-4 [&>svg]:w-4">
+                  <img
+                    src="../../../public/images/Group 1436.svg"
+                    classNameName="220px"
+                    alt="asd"
+                  />
+                </span>
+              </h6>
 
-  <div class="mx-6 py-10 text-center md:text-left">
-    <div class="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4">
-      <div class="">
-        <h6
-          class="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start">
-          <span class="me-3 [&>svg]:h-4 [&>svg]:w-4">
-           <img src="../../../public/images/Group 1436.svg" className="220px" alt="asd" />
-          </span>
-        </h6>
-        <div>
-        <span>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</span>
-        <span className="mt-5">Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor
-          in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam</span>
-        <span>nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis</span>
-           </div>
-      </div>
-      <div>
-        <h6
-          class="mb-4 flex justify-center font-semibold uppercase md:justify-start">
-          Products
-        </h6>
-        <p class="mb-4">
-          <a href="#!">Angular</a>
-        </p>
-        <p class="mb-4">
-          <a href="#!">React</a>
-        </p>
-        <p class="mb-4">
-          <a href="#!">Vue</a>
-        </p>
-        <p>
-          <a href="#!">Laravel</a>
-        </p>
-      </div>
-      <div>
-        <h6
-          class="mb-4 flex justify-center font-semibold uppercase md:justify-start">
-          Useful links
-        </h6>
-        <p class="mb-4">
-          <a href="#!">Pricing</a>
-        </p>
-        <p class="mb-4">
-          <a href="#!">Settings</a>
-        </p>
-        <p class="mb-4">
-          <a href="#!">Orders</a>
-        </p>
-        <p>
-          <a href="#!">Help</a>
-        </p>
-      </div>
-      <div>
-        <h6
-          class="mb-4 flex justify-center font-semibold uppercase md:justify-start">
-          Contact
-        </h6>
-        <p class="mb-4 flex items-center justify-center md:justify-start">
-          <span class="me-3 [&>svg]:h-5 [&>svg]:w-5">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor">
-              <path
-                d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
-              <path
-                d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
-            </svg>
-          </span>
-          New York, NY 10012, US
-        </p>
-        <p class="mb-4 flex items-center justify-center md:justify-start">
-          <span class="me-3 [&>svg]:h-5 [&>svg]:w-5">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor">
-              <path
-                d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
-              <path
-                d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
-            </svg>
-          </span>
-          info@example.com
-        </p>
-        <p class="mb-4 flex items-center justify-center md:justify-start">
-          <span class="me-3 [&>svg]:h-5 [&>svg]:w-5">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor">
-              <path
-                fill-rule="evenodd"
-                d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
-                clip-rule="evenodd" />
-            </svg>
-          </span>
-          + 01 234 567 88
-        </p>
-        <p class="flex items-center justify-center md:justify-start">
-          <span class="me-3 [&>svg]:h-5 [&>svg]:w-5">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor">
-              <path
-                fill-rule="evenodd"
-                d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 003 3h.27l-.155 1.705A1.875 1.875 0 007.232 22.5h9.536a1.875 1.875 0 001.867-2.045l-.155-1.705h.27a3 3 0 003-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0018 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25zM16.5 6.205v-2.83A.375.375 0 0016.125 3h-8.25a.375.375 0 00-.375.375v2.83a49.353 49.353 0 019 0zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 01-.374.409H7.232a.375.375 0 01-.374-.409l.526-5.784a.373.373 0 01.333-.337 41.741 41.741 0 018.566 0zm.967-3.97a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H18a.75.75 0 01-.75-.75V10.5zM15 9.75a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V10.5a.75.75 0 00-.75-.75H15z"
-                clip-rule="evenodd" />
-            </svg>
-          </span>
-          + 01 234 567 89
-        </p>
-      </div>
-    </div>
-  </div>
+              <div className="editwidth w-[494px]">
+                <div className="pb-5">
+                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
+                  diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+                  aliquam erat volutpat .
+                </div>
+                <div classNameName="pb-5">
+                  Ut wisi enim ad minim veniam, quis nostrud exeroi tation
+                  ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+                  consequat. Duis autem vel eum iriure dolor in hendrerit in
+                  vulputate velit esse molestie consequat, vel illum dolore eu
+                  feugiat n ulla. Lorem ipsum dolor sit amet, consectetuer
+                  adipiscing elit, sed diam
+                </div>
 
-  <div class="bg-black/5 p-6 text-center">
-    <span>© 2023 Copyright:</span>
-    <a class="font-semibold" href="https://tw-elements.com/"
-      >TW Elements</a
-    >
-  </div>
-</footer>
+                <div className="mt-5">
+                  nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
+                  erat volutpat. Ut wisi enim ad minim veniam, quis
+                </div>
+              </div>
+            </div>
+
+            <div className="">
+              <label htmlFor="search">
+                Subscribe to our newsletter
+              </label>
+
+              <form class="mt-3">
+                <label
+                  for="default-search"
+                  class="my-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+                >
+                  Search
+                </label>
+                <div class="relative ">
+                  <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"></div>
+                  <input
+                    type="search"
+                    id="default-search"
+                    class="placeholder:text-secondary block w-full p-3 ps-5 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500
+         focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Enter Your Mail"
+                    required
+                  />
+                  <button
+                    type="submit"
+                    class="flex items-center justify-center mb-[-5px] text-black absolute end-2.5 bottom-2.5 bg-primary  
+         font-medium rounded-full w-[120px] text-sm px-4 py-2 "
+                  >
+                    Subscribe
+                    <img className="ms-2" src={footerLogo} alt="" />
+                  </button>
+                </div>
+              </form>
+
+              <div className="dblock  flex justify-between mt-8">
+                <div className="">
+                  <p className="mb-4">
+                    <a href="#!">About Us</a>
+                  </p>
+                  <p className="mb-4">
+                    <a href="#!">Contact Us</a>
+                  </p>
+                  <p className="mb-4">
+                    <a href="#!">Track Order</a>
+                  </p>
+                  <p className="mb-4">
+                    <a href="#!">Terms & Conditions</a>
+                  </p>
+                  <p className="mb-4">
+                    <a href="#!">Privacy Policy</a>
+                  </p>
+                  <p className="mb-4">
+                    <a href="#!">Sell With Us</a>
+                  </p>
+                  <p className="mb-4">
+                    <a href="#!">Shipping And Returns</a>
+                  </p>
+                </div>
+
+
+                <div className="border-left bordernonetwo">
+                  <div className="mb-4 flex justify-between items-center">
+                    <div ><img src={facebook} alt=""/></div>
+                    <div className=" ms-2">/YESHTERY</div>
+                  </div>
+                  <div className="mb-4 flex justify-between items-center">
+                    <div ><img src={linedin} alt=""/></div>
+                    <div className=" ms-2">/YESHTERY</div>
+                  </div>
+                  <div className="mb-4 flex justify-between items-center">
+                    <div ><img src={insta} alt=""/></div>
+                    <div className=" ms-2">/YESHTERY</div>
+                  </div>
+                  <div className="flex justify-between items-center">
+                    <div ><img src={twiteer} alt=""/></div>
+                    <div className=" ms-2">/YESHTERY</div>
+                  </div>
+                </div>
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="dblock container max-w-[1390px] mx-auto border-t py-4 flex justify-between border-[#5F5F5F]">
+
+        <div className="">© 2021 yeshtery, all rights reserved.
+          </div>
+
+
+          <div className="flex justhere">
+          <img  className="ms-2" src={Rectangle} alt="" />
+          <img className="ms-2"  src={visa} alt="" />
+          <img className="ms-2"  src={payment} alt="" />
+          </div>
+
+
+          <div className="flex justify-center items-center">Powered By<img src={nasnavfooter} alt="" /></div>
+
+        </div>
+
+      </footer>
     </>
   );
 }
